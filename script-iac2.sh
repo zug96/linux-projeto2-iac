48808ee63e96e25f48fd2a8b78792fc60b309d1b
zug96 <ccampanagus@gmail.com>
2025-04-27 20:39:32 +0000

    Script: corrige o espaçamento entre as aspas duplas e o comando 'echo'

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo"Atualizando o servidor..."
+echo "Atualizando o servidor..."
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 pat-get install unzip -y
 
-echo"Baixando e copiando os arquivos da aplicação..."
+echo "Baixando e copiando os arquivos da aplicação..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
